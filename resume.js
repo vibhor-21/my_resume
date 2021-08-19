174bf550b38eb00c618c03a6d420f85cc6e3a2e5
Vibhor <47396153+vibhor-21@users.noreply.github.com>
2021-08-20 02:50:55 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,84 @@
+// smooth scroll
+
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var interval;
+
+
+for (var i = 0; i < navMenuAnchorTags.length-1; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
+
+        interval = setInterval(function () {
+            scrollVertically(targetSection);
+        }, 20);
+    });
+}
+
+
+function scrollVertically(targetSection) {
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
+}
+
+
+// dynamic skill container
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
